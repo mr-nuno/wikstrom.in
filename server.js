6beefb04785e7278437eb6d4c632e25d56ddd0e9
Peter Wikström <peter@wikstrom.in>
2014-10-19 15:01:40 +0200

    Chart js

--- server.js
+++ server.js
@@ -1,28 +1,3 @@
-
-/*var port = process.argv.length < 3 ? 8080 : process.argv[2];
-
-var connect = require('connect');
-connect.createServer(
-    connect.static(__dirname)
-).listen(port);
-
-var readline = require('readline');
-
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question("Server running at http://127.0.0.1:" + port + "/\nExit with (x).", function(answer) {
-
-	if (answer === "x") {
-		console.log("Server stopped!");
-  		rl.close();
-  		process.exit();
-	}
-  	
-});*/
-
 var express = require('express');
 var http = require('http');
 var app = express();
